3e8d920d0c0c6574a7e1ba6b23a81aab66a88f0b
A.Harishankar <harishankara583225104034@nprcolleges.org>
2026-08-19 21:15:56 +0530

    Update hypothesis_testing.py

--- Week-1-Data-Preparation/Week-2-Data-Visualization/Week-3-Hypothesis-Testing/hypothesis_testing.py
+++ Week-1-Data-Preparation/Week-2-Data-Visualization/Week-3-Hypothesis-Testing/hypothesis_testing.py
@@ -1,1 +1,64 @@
+import pandas as pd
+from scipy.stats import chi2_contingency
 
+# --------------------------------
+# 1. Load the Titanic dataset
+# --------------------------------
+
+titanic = pd.read_csv("train.csv")
+
+
+# --------------------------------
+# 2. Check the selected columns
+# --------------------------------
+
+print(titanic[["Sex", "Survived"]].head())
+
+print(titanic[["Sex", "Survived"]].isnull().sum())
+
+print(titanic["Sex"].unique())
+
+print(titanic["Survived"].unique())
+
+print(len(titanic))
+
+
+# --------------------------------
+# 3. Create the contingency table
+# --------------------------------
+
+table = pd.crosstab(
+    titanic["Sex"],
+    titanic["Survived"]
+)
+
+print("\nContingency Table:")
+print(table)
+
+
+# --------------------------------
+# 4. Perform Chi-Square Test
+# --------------------------------
+
+chi2, p, dof, expected = chi2_contingency(table)
+
+print("\nChi-Square Statistic:", chi2)
+print("P-value:", p)
+print("Degrees of Freedom:", dof)
+
+print("\nExpected Frequencies:")
+print(expected)
+
+
+# --------------------------------
+# 5. Hypothesis Decision
+# --------------------------------
+
+alpha = 0.05
+
+if p < alpha:
+    print("\nReject H0")
+    print("There is a statistically significant association between gender and survival.")
+else:
+    print("\nFail to reject H0")
+    print("There is no statistically significant association between gender and survival.")
